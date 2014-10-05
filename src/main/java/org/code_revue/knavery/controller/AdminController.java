a8a068774e11ea250c4fb8b958eb6909aee055e8
Mike Fanning <mikegfanning@gmail.com>
2014-10-05 11:36:01 -0700

    Added page for configuring redirect urls.

--- src/main/java/org/code_revue/knavery/controller/AdminController.java
+++ src/main/java/org/code_revue/knavery/controller/AdminController.java
@@ -1,7 +1,14 @@
 package org.code_revue.knavery.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.annotation.Resource;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author Mike Fanning
@@ -9,6 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+
+    @Resource(name = "redirectUrls")
+    private List<String> redirectUrls;
 
     @RequestMapping("/")
     public String overview() {
@@ -41,8 +51,22 @@ public class AdminController {
     }
 
     @RequestMapping("/redirect")
-    public String redirect() {
+    public String redirect(Model model) {
+        model.addAttribute("redirectUrls", Collections.unmodifiableList(redirectUrls));
         return "redirect";
+    }
+
+    @RequestMapping(value = "/redirect/add", method = RequestMethod.POST)
+    public String addRedirect(@RequestParam String url, Model model) {
+        // TODO: Validate this URL dumbshit
+        redirectUrls.add(url);
+        return redirect(model);
+    }
+
+    @RequestMapping(value = "/redirect/remove", method = RequestMethod.POST)
+    public String removeRedirect(@RequestParam String url, Model model) {
+        redirectUrls.remove(url);
+        return redirect(model);
     }
 
 }

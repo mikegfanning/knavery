fb3a46e5bf9bfecc30da22e8a1e829638cf21cc5
Mike Fanning <mikegfanning@gmail.com>
2014-10-18 12:27:23 -0700

    Cleaned up some layout junk and made logging controls ajaxy.

--- src/main/java/org/code_revue/knavery/controller/LoggerController.java
+++ src/main/java/org/code_revue/knavery/controller/LoggerController.java
@@ -4,11 +4,16 @@ import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
+
+import java.util.Collections;
 
 /**
  * @author Mike Fanning
@@ -17,11 +22,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/admin/logging")
 public class LoggerController {
 
+    @Autowired
+    private View jsonView;
+
     // Not great - is there some better way to get all of the Logback Classic levels?
     public static final Level[] LEVELS = new Level[]
             { Level.OFF, Level.ERROR, Level.WARN, Level.INFO, Level.DEBUG, Level.TRACE, Level.ALL };
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home(Model model) {
         LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
         model.addAttribute("loggers", context.getLoggerList());
@@ -30,10 +38,10 @@ public class LoggerController {
     }
 
     @RequestMapping(value = "/level", method = RequestMethod.POST)
-    public String level(@RequestParam String logger, @RequestParam String level, Model model) {
+    public ModelAndView level(@RequestParam String logger, @RequestParam String level) {
         Logger log = (Logger) LoggerFactory.getLogger(logger);
         log.setLevel(Level.toLevel(level));
-        return "redirect:/admin/logging/";
+        return new ModelAndView(jsonView, Collections.singletonMap("status", "success"));
     }
 
 }
